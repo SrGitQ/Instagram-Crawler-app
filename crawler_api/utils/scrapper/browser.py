97a3f5fcbe0e5530d53aeabe81918411185c2941
JMBalanzar <jmbalanzar@gmail.com>
2022-09-12 16:29:13 -0500

    scrapper with new features

--- crawler_api/utils/scrapper/browser.py
+++ crawler_api/utils/scrapper/browser.py
@@ -11,20 +11,20 @@ class InstagramWindow:
     #browser configuration
     route = os.getcwd()
     driver_path = route + driver
-    browser = webdriver.Chrome(driver_path)
-    browser.get("https://www.instagram.com/")
+    self.browser = webdriver.Chrome(driver_path)
+    self.browser.get("https://www.instagram.com/")
 
     #login
-    username = WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='username']")))
-    password = WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='password']")))
+    username = WebDriverWait(self.browser, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='username']")))
+    password = WebDriverWait(self.browser, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='password']")))
     username.clear()
     username.send_keys(user)
     password.clear()
     password.send_keys(pas)
-    WebDriverWait(browser, 2).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']"))).click()
+    WebDriverWait(self.browser, 2).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']"))).click()
     
     #close popups
     time.sleep(2)
-    WebDriverWait(browser, 15).until(EC.element_to_be_clickable((By.XPATH, '//button[contains(text(), "Not Now")]'))).click()
+    WebDriverWait(self.browser, 15).until(EC.element_to_be_clickable((By.XPATH, '//button[contains(text(), "Not Now")]'))).click()
     time.sleep(2)
-    WebDriverWait(browser, 15).until(EC.element_to_be_clickable((By.XPATH, '//button[contains(text(), "Not Now")]'))).click()
+    WebDriverWait(self.browser, 15).until(EC.element_to_be_clickable((By.XPATH, '//button[contains(text(), "Not Now")]'))).click()
